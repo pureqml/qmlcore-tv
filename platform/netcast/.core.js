f5268934c2cb2114370d9b9c070aab6629880554
Alexey Pobery <poberyaa@gmail.com>
2016-10-28 15:50:27 +0300

    Add dict and platform core file.

---
+++ platform/netcast/.core.js
@@ -0,0 +1,46 @@
+if ('NetCastExit' in window) {
+	log = function(dummy) {
+		var args = copyArguments(arguments)
+		console.log("[QML] " + args.join(" "))
+	}
+
+	log("NetCast detected")
+	exports.core.vendor = "LG"
+	exports.core.device = 1
+	exports.core.os = "netcast"
+
+	exports.core.keyCodes = {
+		37: 'Left',
+		38: 'Up',
+		39: 'Right',
+		40: 'Down',
+		13: 'Select',
+		33: 'ChannelUp',
+		34: 'ChannelDown',
+		27: 'Back',
+		19: 'Pause',
+		48: '0',
+		49: '1',
+		50: '2',
+		51: '3',
+		52: '4',
+		53: '5',
+		54: '6',
+		55: '7',
+		56: '8',
+		57: '9',
+		461: 'Back',
+		403: 'Red',
+		404: 'Green',
+		405: 'Yellow',
+		406: 'Blue',
+		457: 'Menu',
+		412: 'Rewind',
+		417: 'FastForward',
+		457: 'Info',
+		413: 'Stop',
+		415: 'Play'
+	}
+
+	log("NetCast initialized")
+}
