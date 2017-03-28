05e1a1fb6068e628b6c6a11b47cde1517ee59409
Alexey Pobery <poberyaa@gmail.com>
2017-03-28 16:49:58 +0300

    Register tizen remote keys explicitly.

--- platform/tizen/.core.js
+++ platform/tizen/.core.js
@@ -1,11 +1,38 @@
 log = console.log.bind(console)
 
-
 if ('tizen' in window) {
 	log("[QML] Tizen")
 	exports.core.vendor = "samsung"
 	exports.core.device = 1
 	exports.core.os = "tizen"
+
+	var inputDevice = window.tizen.tvinputdevice
+	if (inputDevice) {
+		inputDevice.registerKey("0")
+		inputDevice.registerKey("1")
+		inputDevice.registerKey("2")
+		inputDevice.registerKey("3")
+		inputDevice.registerKey("4")
+		inputDevice.registerKey("5")
+		inputDevice.registerKey("6")
+		inputDevice.registerKey("7")
+		inputDevice.registerKey("8")
+		inputDevice.registerKey("9")
+		inputDevice.registerKey("ColorF0Red")
+		inputDevice.registerKey("ColorF1Green")
+		inputDevice.registerKey("ColorF2Yellow")
+		inputDevice.registerKey("ColorF3Blue")
+		inputDevice.registerKey("Menu")
+		inputDevice.registerKey("ChannelUp")
+		inputDevice.registerKey("ChannelDown")
+		inputDevice.registerKey("MediaFastForward")
+		inputDevice.registerKey("MediaPlayPause")
+		inputDevice.registerKey("MediaRewind")
+		inputDevice.registerKey("MediaPlay")
+		inputDevice.registerKey("MediaStop")
+	} else {
+		log('"inputDevice" is undefined dont forget to add privilege: <tizen:privilege name="http://tizen.org/privilege/tv.inputdevice"/> into the "config.xml"')
+	}
 
 	exports.core.keyCodes = {
 		48: '0',
@@ -30,6 +57,11 @@ if ('tizen' in window) {
 		427: 'ChannelUp',
 		428: 'ChannelDown',
 		457: 'Menu',
+		415: 'Play',
+		413: 'Stop',
+		412: 'Rewind',
+		417: 'FastForward',
+		10252: 'Pause',
 		10009: 'Back'
 	}
 
