3d8831b7207a7cc2131654a3934051a4586780a2
Alexey Pobery <poberyaa@gmail.com>
2018-03-14 11:54:43 +0300

    Implement device info filling via hisense API

--- platform/hisense/device.js
+++ platform/hisense/device.js
@@ -1,5 +1,17 @@
 var Device = function(ui) {
-	ui.deviceId = "hisense_" + Math.random().toString(36).substr(2, 9)
+	if (window.Hisense_GetDeviceID)
+		ui.deviceId = window.Hisense_GetDeviceID()
+	else
+		ui.deviceId = "hisense_" + Math.random().toString(36).substr(2, 9)
+
+	if (window.Hisense_GetModelName)
+		ui.modelName = window.Hisense_GetModelName()
+
+	if (window.Hisense_GetFirmWareVersion)
+		ui.firmware = window.Hisense_GetFirmWareVersion()
+
+	if (window.Hisense_GetCountryCode)
+		ui.country = window.Hisense_GetCountryCode()
 }
 
 exports.createDevice = function(ui) {
