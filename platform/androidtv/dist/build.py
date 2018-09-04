c524a1b47cc14a194b433f4c3fc3ff88a0c532ea
Alexey Pobery <poberyaa@gmail.com>
2018-09-04 20:30:25 +0300

    Fix tab format in androidtv build script

--- platform/androidtv/dist/build.py
+++ platform/androidtv/dist/build.py
@@ -5,35 +5,35 @@ import os
 
 
 def build(app, title):
-    os.system('rm -rf %s' %app)
-    res = os.system('cordova create %s com.example.app %s' %(app,title))
-    if res != 0:
-        print "Failed to create android app"
-        return
-    os.system('cp -r `ls -A | grep -v "%s"` %s/www' %(app,app))
-    os.system('cp androidIcon.png %s' %(app))
-    os.system('cp config.xml %s' %(app))
-    os.chdir(app)
-    os.system('cordova platform add android')
+	os.system('rm -rf %s' %app)
+	res = os.system('cordova create %s com.example.app %s' %(app,title))
+	if res != 0:
+		print "Failed to create android app"
+		return
+	os.system('cp -r `ls -A | grep -v "%s"` %s/www' %(app,app))
+	os.system('cp androidIcon.png %s' %(app))
+	os.system('cp config.xml %s' %(app))
+	os.chdir(app)
+	os.system('cordova platform add android')
 
-    os.system('cp ../banner.png ./platforms/android/res/drawable-land-hdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-land-ldpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-land-mdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-land-xhdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-land-xxhdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-land-xxxhdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-port-hdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-port-ldpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-port-mdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-port-xhdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-port-xxhdpi/.')
-    os.system('cp ../banner.png ./platforms/android/res/drawable-port-xxxhdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-land-hdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-land-ldpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-land-mdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-land-xhdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-land-xxhdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-land-xxxhdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-port-hdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-port-ldpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-port-mdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-port-xhdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-port-xxhdpi/.')
+	os.system('cp ../banner.png ./platforms/android/res/drawable-port-xxxhdpi/.')
 
-    os.system('cordova plugin add https://github.com/comrat/Cordova-Android-TV-Plugin')
-    os.system('cordova plugin add cordova-plugin-device')
-    os.system('cordova plugin add cordova-plugin-exoplayer')
-    os.system('cordova build android')
-    os.chdir('..')
+	os.system('cordova plugin add https://github.com/comrat/Cordova-Android-TV-Plugin')
+	os.system('cordova plugin add cordova-plugin-device')
+	os.system('cordova plugin add cordova-plugin-exoplayer')
+	os.system('cordova build android')
+	os.chdir('..')
 
 
 parser = argparse.ArgumentParser('qmlcore build tool')
@@ -44,6 +44,6 @@ args = parser.parse_args()
 
 res = os.system('cordova --version')
 if res == 0:
-    build(args.app, args.title)
+	build(args.app, args.title)
 else:
-    print 'Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/'
+	print 'Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/'
