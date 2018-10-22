20845610cdfcd3d14e49694cb7bd2bbbb537fb0d
Alexey Pobery <poberyaa@gmail.com>
2018-10-23 01:05:43 +0300

    Implement release flag in androidtv build script

--- platform/androidtv/dist/build.py
+++ platform/androidtv/dist/build.py
@@ -4,7 +4,7 @@ import argparse
 import os
 
 
-def build(app, title):
+def build(app, title, release):
 	os.system('rm -rf %s' %app)
 	res = os.system('cordova create %s com.%s.app %s' %(app, app, title))
 	if res != 0:
@@ -34,18 +34,29 @@ def build(app, title):
 	os.system('cordova plugin add cordova-plugin-device')
 	os.system('cordova plugin add cordova-plugin-exoplayer')
 	{% block plugins %}{% endblock %}
-	os.system('cordova build android')
+
+	if release:
+		{% if androidBuild %}
+		build = 'cordova build android --release -- '
+		os.system(build + '--keystore={{androidBuild.keystore}} --storePassword={{androidBuild.storePassword}} --alias={{androidBuild.alias}} --password={{androidBuild.password}}')
+		{% else %}
+		print "Failed to build release apk androidBuild property is undefined"
+		{% endif %}
+	else:
+		os.system('cordova build android')
+
 	os.chdir('..')
 
 
 parser = argparse.ArgumentParser('qmlcore build tool')
 parser.add_argument('--app', '-a', help='application name', default="app")
 parser.add_argument('--title', '-t', help='application title', default="App")
+parser.add_argument('--release', '-r', help='build release apk', default=False)
 args = parser.parse_args()
 
 
 res = os.system('cordova --version')
 if res == 0:
-	build(args.app, args.title)
+	build(args.app, args.title, args.release)
 else:
 	print 'Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/'
