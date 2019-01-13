58e97217c640ba67ad3df7d9c08a8ef6ffb64cd1
Alexey Pobery <poberyaa@gmail.com>
2019-01-13 11:57:08 +0300

    Implement dune device backend

--- platform/dune/device.js
+++ platform/dune/device.js
@@ -1,4 +1,16 @@
 var Device = function(ui) {
+	this._ui = ui
+	this._stb = document.getElementById("duneapi");
+	var stb = this._stb
+	if (!stb) {
+		log("Dune API STB not defined")
+		return
+	}
+	ui.firmware = stb.getFirmwareVersion()
+	ui.deviceId = stb.getSerialNumber()
+	ui.macAddress = stb.getMacAddress()
+	ui.sdk = stb.getApiVersion()
+	ui.modelName = stb.getProductId()
 }
 
 exports.createDevice = function(ui) {
