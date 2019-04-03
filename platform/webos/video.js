008730dd8fb812e3ddab7e62c9bab6f703bc5469
Alexey Pobery <poberyaa@gmail.com>
2019-04-03 21:16:59 +0300

    Use event listners from html5 player

--- platform/webos/video.js
+++ platform/webos/video.js
@@ -4,10 +4,10 @@ var Player = function(ui) {
 
 	var dom = player.dom
 	var self = this
-	player.on('play', function() { ui.waiting = false; ui.paused = dom.paused }.bind(ui))
-	player.on('pause', function() { ui.paused = dom.paused }.bind(ui))
-	player.on('ended', function() { ui.finished() }.bind(ui))
-	player.on('seeked', function() { log("seeked"); ui.seeking = false; ui.waiting = false }.bind(ui))
+
+	this.element = player
+	this.ui = ui
+	this.setEventListeners()
 
 	player.on('canplay', function() {
 		log("canplay", dom.readyState);
@@ -18,66 +18,6 @@ var Player = function(ui) {
 			self._seekAfterSwitchProgress = undefined
 		}
 	}.bind(ui))
-
-	player.on('seeking', function() { log("seeking"); ui.seeking = true; ui.waiting = true }.bind(ui))
-	player.on('waiting', function() { log("waiting"); ui.waiting = true }.bind(ui))
-	player.on('emptied', function() { log("Was emptied", dom.networkState); if (!dom.paused) dom.play()}.bind(ui))
-	player.on('volumechange', function() { ui.muted = dom.muted }.bind(ui))
-	player.on('canplaythrough', function() { log("ready to play"); ui.paused = dom.paused }.bind(ui))
-
-	player.on('stalled', function() {
-		log("Was stalled", dom.networkState)
-		//TODO: trye to realod content if its really stuck
-	}.bind(ui))
-
-	player.on('error', function() {
-		log("Player error occured", dom.error, "src", ui.source)
-		if (!dom.error || !ui.source)
-			return
-
-		ui.error(dom.error)
-
-		log("player.error", dom.error)
-		switch (dom.error.code) {
-		case 1:
-			log("MEDIA_ERR_ABORTED error occured")
-			break;
-		case 2:
-			log("MEDIA_ERR_NETWORK error occured")
-			break;
-		case 3:
-			log("MEDIA_ERR_DECODE error occured")
-			break;
-		case 4:
-			log("MEDIA_ERR_SRC_NOT_SUPPORTED error occured")
-			break;
-		default:
-			log("UNDEFINED error occured")
-			break;
-		}
-	}.bind(ui))
-
-	player.on('timeupdate', function() {
-		ui.waiting = false
-		if (!ui.seeking)
-			ui.progress = dom.currentTime
-	}.bind(ui))
-
-	player.on('durationchange', function() {
-		var d = dom.duration
-		log("Duration", d)
-		ui.duration = isFinite(d) ? d : 0
-	}.bind(ui))
-
-	player.on('progress', function() {
-		var last = dom.buffered.length - 1
-		ui.waiting = false
-		if (last >= 0)
-			ui.buffered = dom.buffered.end(last) - dom.buffered.start(last)
-	}.bind(ui))
-
-	this.element = player
-	this.ui = ui
 
 	ui.element.remove()
 	ui.element = player
