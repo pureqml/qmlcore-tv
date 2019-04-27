03567b4c3b1f167633f0ee442b85ca8579357fca
Alexey Pobery <poberyaa@gmail.com>
2019-04-27 15:56:47 +0300

    Try to fit the screen on device ready event occured insteda of hardcoded timeout

--- platform/androidtv/.core.js
+++ platform/androidtv/.core.js
@@ -3,27 +3,6 @@ _globals.core.__deviceBackend = function() { return _globals.androidtv.device }
 
 exports.core.device = 1
 
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-	_globals._context.system.vendor = device.manufacturer
-}
-
-document.addEventListener("click", handler, true);
-function handler(e){
-	e.stopPropagation();
-	e.preventDefault();
-}
-
-window.cordovaExecCall = function(name, args, callback, error) {
-	log("cordovaExecCall", name, "args", args)
-	if (window.cordova) {
-		window.cordova.exec(callback, error, "AndroidTV", name, args);
-	} else {
-		log("Cordova undefined error")
-		error()
-	}
-};
-
 var fitToScreen = function() {
 	var width = window.$manifest$resolutionWidth
 	var height = window.$manifest$resolutionHeight
@@ -41,6 +20,27 @@ var fitToScreen = function() {
 	}
 }
 
-setTimeout(fitToScreen, 500)
+window.cordovaExecCall = function(name, args, callback, error) {
+	log("cordovaExecCall", name, "args", args)
+	if (window.cordova) {
+		window.cordova.exec(callback, error, "AndroidTV", name, args);
+	} else {
+		log("Cordova undefined error")
+		error()
+	}
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+	_globals._context.system.vendor = device.manufacturer
+	console.log("DEVICEREDA")
+	fitToScreen()
+}
+
+document.addEventListener("click", handler, true);
+function handler(e){
+	e.stopPropagation();
+	e.preventDefault();
+}
 
 log("AndroidTV initialized")
