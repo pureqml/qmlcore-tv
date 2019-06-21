b80dc19aad4fe9f74d4c771c62d543004d0b89ee
Alexey Pobery <poberyaa@gmail.com>
2019-06-21 12:18:53 +0300

    Move 'log' method definition and key codes map out of orsay platform scope

--- platform/orsay/.core.js
+++ platform/orsay/.core.js
@@ -5,16 +5,16 @@ var widgetAPI
 var tvKey
 var pluginAPI
 
+log = function(dummy) {
+	COPY_ARGS(args, 0)
+	alert("[QML] " + args.join(" "))
+}
+
 if ('Common' in window) {
 	alert("[QML] samsung smart tv")
 	exports.core.vendor = "samsung"
 	exports.core.device = 1
 	exports.core.os = "orsay"
-
-	log = function(dummy) {
-		COPY_ARGS(args, 0)
-		alert("[QML] " + args.join(" "))
-	}
 
 	log("loading")
 	widgetAPI = new window.Common.API.Widget() // Creates Common module
@@ -37,42 +37,42 @@ if ('Common' in window) {
 		context._processActions();
 	}
 
-	exports.core.keyCodes = {
-		4: 'Left',
-		5: 'Right',
-		17: '0',
-		101: '1',
-		98: '2',
-		6: '3',
-		8: '4',
-		9: '5',
-		10: '6',
-		12: '7',
-		13: '8',
-		14: '9',
-		20: 'Green',
-		21: 'Yellow',
-		22: 'Blue',
-		68: 'PageUp',
-		65: 'PageDown',
-		68: 'ChannelUp',
-		65: 'ChannelDown',
-		69: 'Rewind',
-		70: 'Stop',
-		71: 'Play',
-		72: 'FastForward',
-		74: 'Pause',
-		88: 'Back',
-		108: 'Red',
-		262: 'Menu',
-		29461: 'Down',
-		29460: 'Up',
-		29443: 'Select'
-	}
-
 	exports.closeApp = function() {
 		widgetAPI.sendExitEvent();
 	}
 
 	log("Orsay initialized")
+}
+
+exports.core.keyCodes = {
+	4: 'Left',
+	5: 'Right',
+	17: '0',
+	101: '1',
+	98: '2',
+	6: '3',
+	8: '4',
+	9: '5',
+	10: '6',
+	12: '7',
+	13: '8',
+	14: '9',
+	20: 'Green',
+	21: 'Yellow',
+	22: 'Blue',
+	68: 'PageUp',
+	65: 'PageDown',
+	68: 'ChannelUp',
+	65: 'ChannelDown',
+	69: 'Rewind',
+	70: 'Stop',
+	71: 'Play',
+	72: 'FastForward',
+	74: 'Pause',
+	88: 'Back',
+	108: 'Red',
+	262: 'Menu',
+	29461: 'Down',
+	29460: 'Up',
+	29443: 'Select'
 }
