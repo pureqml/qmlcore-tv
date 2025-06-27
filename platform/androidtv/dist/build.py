5d2fce6a73dcf896b82a70585b030a8be9755462
Денис Комсиков <deniskomsikov@gmail.com>
2025-06-27 20:51:30 +0300

    Fix pureqml to android build with cordova

--- platform/androidtv/dist/build.py
+++ platform/androidtv/dist/build.py
@@ -8,31 +8,23 @@ def build(app, title, release):
 	os.system('rm -rf %s' %app)
 	res = os.system('cordova create %s com.%s.app %s' %(app, app, title))
 	if res != 0:
-		print "Failed to create android app"
+		print("Failed to create android app")
 		return
-	os.system('rsync -a ./ %s/www --exclude=%s ' %(app,app))
+	os.system('rsync -a --remove-source-files ./ %s/www --exclude=%s' %(app,app))
 	os.system('cp androidIcon.png %s' %(app))
 	os.system('cp config.xml %s' %(app))
 	os.chdir(app)
 	os.system('cordova platform add android')
 	{% block commands %}{% endblock %}
 
-	os.system('cp ../banner.png ./platforms/android/res/drawable-land-hdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-land-ldpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-land-mdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-land-xhdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-land-xxhdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-land-xxxhdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-port-hdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-port-ldpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-port-mdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-port-xhdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-port-xxhdpi/.')
-	os.system('cp ../banner.png ./platforms/android/res/drawable-port-xxxhdpi/.')
-
-	os.system('cordova plugin add https://github.com/comrat/Cordova-Android-TV-Plugin')
+	# os.system('cordova plugin add https://github.com/comrat/Cordova-Android-TV-Plugin')
 	os.system('cordova plugin add cordova-plugin-device')
-	os.system('cordova plugin add https://github.com/comrat/cordova-plugin-exoplayer')
+	# os.system('cordova plugin add https://github.com/comrat/cordova-plugin-exoplayer')
+	os.system('cordova plugin add cordova-plugin-screen-orientation')
+	os.system('cordova plugin add cordova-plugin-exoplayer-smart')
+	# os.system('cordova plugin add cordova-plugin-android-tv')
+	os.system('git clone https://gitlab.tvigle.ru/pubprojects/cordova-plugin-android-tv cordova-plugin-android-tv')
+	os.system('rm -rf cordova-plugin-android-tv')
 	{% block plugins %}{% endblock %}
 
 	if release:
@@ -40,7 +32,7 @@ def build(app, title, release):
 		build = 'cordova build android --release -- '
 		os.system(build + '--keystore={{androidBuild.keystore}} --storePassword={{androidBuild.storePassword}} --alias={{androidBuild.alias}} --password={{androidBuild.password}}')
 		{% else %}
-		print "Failed to build release apk androidBuild property is undefined"
+		print("Failed to build release apk androidBuild property is undefined")
 		{% endif %}
 	else:
 		os.system('cordova build android')
@@ -59,4 +51,4 @@ res = os.system('cordova --version')
 if res == 0:
 	build(args.app, args.title, args.release)
 else:
-	print 'Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/'
+	print('Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/')
